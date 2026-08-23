10fd62614522081c091c31230acb9fb535943c3d
Pedro Bezerra <dulce.leia06@gmail.com>
2026-08-23 17:05:09 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: "es2022",
-    cssCodeSplit: true,
-  },
+    plugins: [react()],
+    base: "/pedro-portifolio/",
 });
